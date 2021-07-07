ca1751c9d8734caf0710c30a41c5bbe7c237bb79
seiya1911 <n.siy1911cb@gmail.com>
2021-07-07 06:38:16 +0000

    [Add]views/drawers

--- app/controllers/drawers_controller.rb
+++ app/controllers/drawers_controller.rb
@@ -1,2 +1,44 @@
 class DrawersController < ApplicationController
+
+  def index
+    @drawers = Drawer.all
+  end
+
+  def show
+    @drawer = Drawer.find(params[:id])
+  end
+
+  def new
+    @drawer = Drawer.new
+  end
+
+  def create
+    @drawer = Drawer.new(drawer_params)
+    @drawer.user_id = current_user.id
+    if @drawer.save
+      redirect_to drawer_path(@drawer), notice: '新規Drawerを作成しました。'
+    else
+      @drawers = drawer.all
+      flash.now[:danger] = '新規作成に失敗しました。'
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @drawer.update(drawer_params)
+      redirect_to drawer_path(@drawer), notice: 'Drawerを更新しました。'
+    else
+      flash.now[:danger] = '更新に失敗しました。'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def drawer_params
+    params.require(:drawer).permit(:name, :image_id, :description)
+  end
 end
