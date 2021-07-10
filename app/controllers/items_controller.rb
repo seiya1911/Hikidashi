a8a1a4a59dba5b4b034c1b1759b5795b174652a2
seiya1911 <n.siy1911cb@gmail.com>
2021-07-10 10:26:13 +0000

    [Add]items_update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
-    @item_tags = @item.tags 
+    @item_tags = @item.tags
   end
 
   def new
+    @item = Item.new
   end
 
   def create
@@ -12,16 +13,32 @@ class ItemsController < ApplicationController
     tag_list = params[:item][:tag_name].split(nil)
     if @item.save
       @item.save_tag(tag_list)
-      redirect_back(fallback_location: root_path)
+      redirect_to item_path(@item), notice: 'Itemを作成しました。'
     else
-      redirect_back(fallback_location: root_path)
+      flash.now[:danger] = '作成に失敗しました。'
+      render 'new'
     end
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @item_tags = @item.tags
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item_tags = @item.tags
+    tag_list = params[:item][:tag_name].split(nil)
+    if @item.update
+      @item.save_tag(tag_list)
+      redirect_ot item_path(@item), notice: 'Itemを作成しました。'
+    else
+      @item = Item.find(params[:id])
+      @item_tags = @item.tags
+      tag_list = params[:item][:tag_name].split(nil)
+      flash.now[:danger] = '作成に失敗しました。'
+      render 'edit'
+    end
   end
 
   private
