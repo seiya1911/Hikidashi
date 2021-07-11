516d1592e647ec24d5e5cc5d34ca6a18d3194ba8
seiya1911 <n.siy1911cb@gmail.com>
2021-07-11 05:45:57 +0000

    [Fix]new_registration_path

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
-         validates :name, presence: true
+
+  validates :name, presence: true
+
   # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -12,10 +14,10 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
     end
   end
-  
+
   has_many :drawers, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :tags, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+
 end
